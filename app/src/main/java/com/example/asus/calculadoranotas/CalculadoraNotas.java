f257cc793fe892200120b93534a8cfc38b0e89f5
Gabrielamc24 <gabrielamc2820@gmail.com>
2019-08-10 13:39:39 -0500

    prueba de subida

---
+++ app/src/main/java/com/example/asus/calculadoranotas/CalculadoraNotas.java
@@ -0,0 +1,13 @@
+package com.example.asus.calculadoranotas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculadoraNotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora_notas);
+    }
+}
